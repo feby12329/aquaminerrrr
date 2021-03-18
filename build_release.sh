25df4ad9f8d6a9abc09b52fb67b26a9d53828c41
aerth <aerth@riseup.net>
2021-03-18 20:11:36 +0000

    reorder build, avx2 first

--- build_release.sh
+++ build_release.sh
@@ -4,15 +4,15 @@ mkdir aquachain-miner # should not exist
 make clean && make -C aquahash clean
 
 # build each, copy to folder and clean afterwards
-#make -j 4 config=plain
-#mv bin/* aquachain-miner/
-#make clean && make -C aquahash clean
-#make -j 4 config=avx
-#mv bin/* aquachain-miner/
-#make clean && make -C aquahash clean
 make -j 4 config=avx2
 mv bin/* aquachain-miner/
-#make clean && make -C aquahash clean
+make clean && make -C aquahash clean
+make -j 4 config=plain
+mv bin/* aquachain-miner/
+make clean && make -C aquahash clean
+make -j 4 config=avx
+mv bin/* aquachain-miner/
+make clean && make -C aquahash clean
 
 tar czvf aquachain-miner-linux-amd64.tar.gz aquachain-miner/
 ls -thalr aquachain-miner-linux-amd64.tar.gz
